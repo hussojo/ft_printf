a5f8aa7d8152ab9b8c3c69eee09030f42814e12d
Josefina Husso <jhusso@c2r5p7.hive.fi>
2022-12-17 16:17:59 +0200

    working on int min with d and i

--- main.c
+++ main.c
@@ -1,15 +1,14 @@
 #include "ft_printf.h"
-# define INT_MIN -2147483648
 
 int main(void)
 {
-	// char res_o;
-	char res_r;
+	char res_o;
+	// char res_r;
 
-	// res_o = ft_printf(",oi %i moi\n", INT_MIN);
-	// printf("%d\n", res_o);
-	res_r = printf(",oi %i moi\n", INT_MIN);
-	printf("%d\n", res_r);
+	res_o = ft_printf(",oi %i moi\n", -2147483648);
+	printf("%d\n", res_o);
+	// res_r = printf(",oi %i moi\n", -2147483648);
+	// printf("%d\n", res_r);
 
 	return(0);
 }
